b83f25052c55cfeb21b06f91216ba0915a9225b4
Hugo Yeung <hugoymh@gmail.com>
2018-03-01 10:57:38 +0800

    Added Skybox objects to System prefab

---
+++ Assets/Mymy/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    private Scene scene;
+    private VideoPlayer video;
+    private Animator anim;
+    private Image fadeImage;
+
+    [Header("Scene Management")]
+    public string nextScene;
+    public string ActiveScene
+    {
+        get { return activeScene; }
+    }
+    private string activeScene;
+
+    [Space]
+    [Header("UI Settings")]
+
+    public bool useFade;
+    public GameObject fadeOverlay;
+    public GameObject ControlUI;
+
+    //make sure that we only have a single instance of the game manager
+    void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Find the video in the scene and pause it
+    public void PauseVideo()
+    {
+        if (!video)
+        {
+            video = FindObjectOfType<VideoPlayer>();
+        }
+        video.Pause();
+    }
+
+    //Find the video in the scene and play it
+    public void PlayVideo()
+    {
+        if (!video)
+        {
+            video = FindObjectOfType<VideoPlayer>();
+        }
+        video.Play();
+    }
+}
