e8457f2317f3a6d5fb3bbe92bdab7797b68f78e5
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-03-02 12:59:30 +0800

    TrimmedGoEvent

---
+++ Assets/Mymy/Scripts/Testing/TrimmedGoEvent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TrimmedGoEvent : MonoBehaviour, 
+    IHandlePointer
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown: TrimmedGoEvent");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerEnter: TrimmedGoEvent");
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            meshRenderer.material.SetColor("_Color", Color.yellow);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerExit: TrimmedGoEvent");
+
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            meshRenderer.material.SetColor("_Color", Color.red);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown: TrimmedGoEvent");
+    }
+}
