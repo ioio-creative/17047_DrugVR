45f1a522f84ac9529f34e24fc6f222f44967184f
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-03-02 21:58:37 +0800

    Added custom pointer to controller

---
+++ Assets/Mymy/Scripts/AssignControllerToEventSystem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AssignControllerToEventSystem : MonoBehaviour
+{
+    private void Start()
+    {
+        WaveVR_ControllerInputModule evntSys = GameObject.Find("/System/EventSystem").GetComponent<WaveVR_ControllerInputModule>();
+        evntSys.RightController = gameObject;     
+    }
+}
