3451cd6eaa16126fc9b3869048fa440204c7f0a1
Hugo Yeung <hugoymh@gmail.com>
2018-03-08 17:54:10 +0800

    PickableContainer

--- Assets/Mymy/Scripts/PickableConfinedToPlane.cs
+++ Assets/Mymy/Scripts/PickableConfinedToPlane.cs
@@ -15,13 +15,16 @@ public class PickableConfinedToPlane : MonoBehaviour
     [SerializeField]
     private float m_MaxPointerDist = 100f;
 
+    public GameObject m_PickableContainer;
+    public GameObject m_PickupContainer;
     
     private Transform m_DebugLaserTransform;    
     private GameObject m_RaycastOriginObject;
     private Rigidbody m_ThisRigidBody;
     private LayerMask m_ConfinedPlaneLayer;
 
-
+    private Vector3 m_GrabPosOffset;
+    private Vector3 m_GrabRotOffset;
     /* MonoBehaviour */
 
     private void Awake()
@@ -55,10 +58,14 @@ public class PickableConfinedToPlane : MonoBehaviour
 
             foreach (RaycastHit hit in hits)
             {
-                // hit corresponding to this gameObject
+                // hit corresponding to confined plane
                 if (hit.collider == m_ConfinedPlane)
                 {
-                    gameObject.transform.position = hit.point;
+                    m_PickupContainer.transform.position = hit.point;
+                    m_PickupContainer.transform.rotation = m_RaycastOriginObject.transform.rotation;
+
+
+                    //gameObject.transform.position = hit.point + m_GrabPosOffset;                  
 
                     if (m_IsShowDebugRay)
                     {
@@ -84,12 +91,44 @@ public class PickableConfinedToPlane : MonoBehaviour
     {        
         m_RaycastOriginObject = raycastOriginObject;
 
+        // !!! Important !!!
+        // use RaycastAll
+        RaycastHit[] hits = Physics.RaycastAll(m_RaycastOriginObject.transform.position,
+            m_RaycastOriginObject.transform.forward,
+            m_MaxPointerDist, m_ConfinedPlaneLayer);
+
+        foreach (RaycastHit hit in hits)
+        {
+            // hit corresponding to confined plane
+            if (hit.collider == m_ConfinedPlane)
+            {
+                m_PickupContainer.transform.position = hit.point;
+                m_PickupContainer.transform.rotation = m_RaycastOriginObject.transform.rotation;
+                gameObject.transform.parent = m_PickupContainer.transform;
+
+                //m_GrabPosOffset = gameObject.transform.position - hit.point;
+                //gameObject.transform.position = hit.point + m_GrabPosOffset;
+
+                //m_GrabRotOffset = gameObject.transform.rotation.eulerAngles - m_RaycastOriginObject.transform.rotation.eulerAngles;
+                //gameObject.transform.rotation.eulerAngles = m_RaycastOriginObject.transform.rotation.eulerAngles + m_GrabRotOffset;
+
+                if (m_IsShowDebugRay)
+                {
+                    ShowLaser(hit, m_RaycastOriginObject.transform);
+                }
+
+                break;
+            }
+        }
+
         // make the object not respond to physics
         m_ThisRigidBody.isKinematic = true;        
     }
 
     public void OnObjectReleased()
     {
+        gameObject.transform.parent = m_PickableContainer.transform;
+        
         // make the object respond to physics
         m_ThisRigidBody.isKinematic = false;        
         
