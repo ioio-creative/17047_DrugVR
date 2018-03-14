6029fd98b9fc79560f885f144dcf436826b427e1
Hugo Yeung <hugoymh@gmail.com>
2018-03-14 12:47:11 +0800

    lighter and controller model conflict solved

--- Assets/Lighter_with_Flame_FX/Scripts/Lighter.cs
+++ Assets/Lighter_with_Flame_FX/Scripts/Lighter.cs
@@ -2,61 +2,63 @@ using UnityEngine;
 using System.Collections;
 using wvr;
 
-public class Lighter : MonoBehaviour {
-
-public GameObject LighterMesh;
-public GameObject FlameFX;
-public GameObject FlameLight;
-public AudioSource LighterIgniteAudio;
-public AudioSource LighterAudio;
+public class Lighter : MonoBehaviour
+{
+    public GameObject LighterMesh;
+    public GameObject FlameFX;
+    public GameObject FlameLight;
+    public AudioSource LighterIgniteAudio;
+    public AudioSource LighterAudio;
   
-void Start (){
-
-    FlameFX.SetActive(false);
-    FlameLight.SetActive(false);
-
-}  
-  
-  
-void Update (){
- 
-	if (Input.GetButtonDown("Fire1") || WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right).GetPressDown(WVR_InputId.WVR_InputId_17)) //check to see if the left mouse was pressed
+    void Start ()
     {
-		StartCoroutine("LighterOn");
+
+        FlameFX.SetActive(false);
+        FlameLight.SetActive(false);
+
+    }  
+  
+  
+    void Update ()
+    {
+
+	    if (Input.GetButtonDown("Fire1") || WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right).GetPressDown(WVR_InputId.WVR_InputId_Alias1_Touchpad)) //check to see if the left mouse was pressed
+        {
+		    StartCoroutine("LighterOn");
        
          
+        }
+        if (Input.GetButtonUp("Fire1") || WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right).GetPressUp(WVR_InputId.WVR_InputId_Alias1_Touchpad))
+        {
+
+        LighterMesh.GetComponent<Animation>().Play("Release Button");
+
+        FlameFX.SetActive(false);
+
+        FlameLight.SetActive(false);
+
+        LighterAudio.Stop();
+
+        }
+         
     }
-    if (Input.GetButtonUp("Fire1") || WaveVR_Controller.Input(WVR_DeviceType.WVR_DeviceType_Controller_Right).GetPressUp(WVR_InputId.WVR_InputId_17))
+ 
+ 
+    IEnumerator LighterOn ()
     {
 
-    LighterMesh.GetComponent<Animation>().Play("Release Button");
+         LighterIgniteAudio.Play();
 
-    FlameFX.SetActive(false);
+         LighterAudio.Play();
 
-    FlameLight.SetActive(false);
+         LighterMesh.GetComponent<Animation>().Play("Push Button");
 
-    LighterAudio.Stop();
+         FlameFX.SetActive(true);
+    
+         FlameLight.SetActive(true);
+
+	     yield return new WaitForSeconds (2);
 
     }
-
-            
- }
- 
- 
-IEnumerator LighterOn (){
-
-     LighterIgniteAudio.Play();
-
-     LighterAudio.Play();
-
-     LighterMesh.GetComponent<Animation>().Play("Push Button");
-
-     FlameFX.SetActive(true);
-    
-     FlameLight.SetActive(true);
-
-	 yield return new WaitForSeconds (2);
-
- }
 
 }
---
+++ Assets/Mymy/Scripts/ControllerAddOns/AddLighterToFocusController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddLighterToFocusController : MonoBehaviour
+{
+    public MY_WaveVR_ControllerPoseTracker FocusControllerGO;
+    public GameObject OriginalControllerModel;
+    public LaserPointer ControllerLaser;
+    
+    // Use this for initialization
+	void Awake ()
+    {
+        gameObject.transform.parent = FocusControllerGO.transform;
+        OriginalControllerModel.SetActive(false);
+        ControllerLaser.enabled = false;
+        FocusControllerGO.lockRotation = true;
+        OriginalControllerModel.transform.localScale = Vector3.zero;
+
+    }
+
+    private void OnDestroy()
+    {
+        OriginalControllerModel.SetActive(true);
+        OriginalControllerModel.transform.localScale = Vector3.one;
+        ControllerLaser.enabled = true;
+        FocusControllerGO.lockRotation = false;
+    }
+}
