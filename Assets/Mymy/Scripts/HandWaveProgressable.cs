07d71864cf66e48b3938da0edf2fc7d108557300
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2018-03-14 16:52:50 +0800

    Hand Wave mechanism

---
+++ Assets/Mymy/Scripts/HandWaveProgressable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandWaveProgressable : MonoBehaviour
+{
+    public event Action OnProgressComplete;
+
+
+    // 0 <= value to return <= 1
+    public float Progress
+    {
+        get
+        {
+            return m_ProgressBar.fillAmount;
+        }       
+    }
+
+
+    private const float MinValue = 0f;
+    private const float MaxValue = 1f;
+
+
+    [SerializeField]
+    private Image m_ProgressBar;
+    [SerializeField]
+    private int m_NumOfStepsToCompletion;
+
+    private float m_ProgressPerStep;
+
+
+    private void Start()
+    {
+        Reset();
+        m_ProgressPerStep = (MaxValue - MinValue) / m_NumOfStepsToCompletion;
+    }
+
+    public void Reset()
+    {
+        m_ProgressBar.fillAmount = MinValue;
+    }
+
+    public void StepIt()
+    {
+        m_ProgressBar.fillAmount += m_ProgressPerStep;
+        if (m_ProgressBar.fillAmount >= MaxValue)
+        {
+            if (OnProgressComplete != null)
+            {
+                OnProgressComplete();
+            }
+        }
+    }
+}
---
+++ Assets/Mymy/Scripts/PrintOrientation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class PrintOrientation : MonoBehaviour
+{
+    public float Zenith = 0f;
+    public float Azimuth = 0f;
+
+
+    private static Vector3 StaticUp = Vector3.up;
+    private static Vector3 StaticForward = Vector3.forward;
+    private static Vector3 StaticRight = Vector3.right;
+
+
+    private void Start()
+    {
+		
+	}
+		
+	private void Update()
+    {        
+        Vector3 forwardVec = transform.forward;
+        Zenith = Mathf.Rad2Deg * Mathf.Acos(Vector3.Dot(forwardVec, StaticUp));
+
+        Vector3 normedProjectionOnFloor = Vector3.Normalize(transform.forward - new Vector3(0, transform.forward.y, 0));
+        float signedMagnitudeOfSineAzimuth = Vector3.Dot(Vector3.Cross(StaticForward, normedProjectionOnFloor), StaticUp);
+        Azimuth = Mathf.Rad2Deg * Mathf.Asin(signedMagnitudeOfSineAzimuth);
+
+        Debug.DrawRay(transform.position, StaticUp, Color.green);
+        Debug.DrawRay(transform.position, StaticForward, Color.yellow);
+        Debug.DrawRay(transform.position, StaticRight, Color.red);
+
+        Debug.DrawRay(transform.position, forwardVec, Color.blue);
+        Debug.DrawRay(transform.position, normedProjectionOnFloor, Color.black);
+	}
+}
