0d8bbaaa9048db982a720bf44e55bda07cb68707
Hugo Yeung <hugoymh@gmail.com>
2018-04-30 17:08:06 +0800

    GUI Radial Prefab Update

--- Assets/Mymy/Scripts/ByScenes/03A/Sc03AClient.cs
+++ Assets/Mymy/Scripts/ByScenes/03A/Sc03AClient.cs
@@ -7,10 +7,38 @@ public class Sc03AClient : MonoBehaviour
 {
     //private DrugVR_SceneENUM nextSceneToLoad;
     private GameManager managerInst;
+    [SerializeField]
+    private Material introSphere;
+    [SerializeField]
+    private Animator m_sphereAnim;
+    [SerializeField]
+    [Range(0,1.0f)]
+    private float sphereAlpha = 1.0f;
+    private bool sphereFadeOutSwitch = false;
 
     private void Awake()
     {
         managerInst = GameManager.Instance;
+    }
+
+    private void Start()
+    {
+        if (introSphere == null)
+        {
+            introSphere = GetComponentInChildren<MeshRenderer>().material;
+        }
+        //FadeOutSphere();
+    }
+
+    private void FixedUpdate()
+    {
+        if (sphereFadeOutSwitch) SetSphereOpacity(sphereAlpha);
+
+        if (sphereAlpha <= 0)
+        {
+            sphereFadeOutSwitch = false;
+            m_sphereAnim.ResetTrigger("FadeOutSphere");
+        }
     }
 
     private void GoToSceneOnChoice()
@@ -24,5 +52,17 @@ public class Sc03AClient : MonoBehaviour
             managerInst.GoToScene(DrugVR_SceneENUM.Sc01B);
         }
     }
+
+    private void FadeOutSphere()
+    {
+        sphereFadeOutSwitch = true;
+        m_sphereAnim.SetTrigger("FadeOutSphere");
+    }
+
+    private void SetSphereOpacity(float alpha)
+    {
+        introSphere.SetFloat("_Transparency", alpha);
+    }
+
 
 }
