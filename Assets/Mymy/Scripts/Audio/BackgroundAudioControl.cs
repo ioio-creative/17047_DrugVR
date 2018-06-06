582e1c8c9d23b7a6c7f481d48f41c8ee2ffd51cf
Hugo Yeung <hugoymh@gmail.com>
2018-06-06 12:20:36 +0800

    update alpha videos with sound

---
+++ Assets/Mymy/Scripts/Audio/BackgroundAudioControl.cs
@@ -0,0 +1,35 @@
+﻿using DrugVR_Scribe;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAudioControl : MonoBehaviour
+{
+    public enum AudioLoopType
+    {
+        Music,
+        Ambience
+    }
+
+    public static BackgroundAudioControl Instance = null;
+
+    [SerializeField]
+    private AudioLoopPackage[] audioLoops;
+
+    [Serializable]
+    public class AudioLoopPackage
+    {
+        public AudioLoopType LoopType;
+        public AudioClip Clip;
+        public DrugVR_SceneENUM SceneToStartLoop;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+}
