056bb1a49e158d0d35b3f9a182772cd2980da453
Hugo Yeung <hugoymh@gmail.com>
2018-06-06 17:49:44 +0800

    Audio transition upon OnSceneChange

--- Assets/Mymy/Scripts/Audio/BackgroundAudioControl.cs
+++ Assets/Mymy/Scripts/Audio/BackgroundAudioControl.cs
@@ -2,34 +2,120 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class BackgroundAudioControl : MonoBehaviour
 {
     public enum AudioLoopType
     {
-        Music,
-        Ambience
+        Ambience,
+        Music        
     }
 
     public static BackgroundAudioControl Instance = null;
 
     [SerializeField]
-    private AudioLoopPackage[] audioLoops;
+    private AudioLoopPackage[] m_AudioLoops;
 
     [Serializable]
     public class AudioLoopPackage
     {
         public AudioLoopType LoopType;
         public AudioClip Clip;
-        public DrugVR_SceneENUM SceneToStartLoop;
+        public DrugVR_SceneENUM[] ScenesToLoop;
     }
+
+    [SerializeField]
+    private AudioSource m_AmbAudioSrc;
+    [SerializeField]
+    private AudioSource m_MusicAudioSrc;
 
     private void Awake()
     {
         if (Instance == null)
         {
-            Instance = this;
+            Instance = this;          
         }
+        else if (Instance != this)
+        {
+            Destroy(this);
+        }
+        
+        m_AmbAudioSrc.volume = 0;
+        m_AmbAudioSrc.mute = true;
+        m_AmbAudioSrc.loop = true;
+        m_MusicAudioSrc.volume = 0;
+        m_AmbAudioSrc.mute = true;
+        m_MusicAudioSrc.loop = true;
+    }
+
+    private void OnEnable()
+    {
+        GameManager.OnSceneChange += HandleSceneChange;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.OnSceneChange -= HandleSceneChange;
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("BGACtrl Destroy");
+    }
+
+    private void HandleSceneChange(DrugVR_SceneENUM nextScene)
+    {
+        IEnumerable<AudioLoopPackage> applicableAudioLoops =
+            m_AudioLoops.Where(x => x.ScenesToLoop.Contains(nextScene));
+
+
+        //We check if AudioLoopPackage is applicable to this scene, including cases when jumping scenes via menu
+        foreach (AudioLoopPackage applicableAudioLoop in applicableAudioLoops)
+        {
+            AudioSource _audioSrc;
+            //TODO Play loop
+            switch (applicableAudioLoop.LoopType)
+            {
+                case AudioLoopType.Ambience:
+                default:
+                    _audioSrc = m_AmbAudioSrc;
+                    break;
+                case AudioLoopType.Music:
+                    _audioSrc = m_MusicAudioSrc;
+                    break;
+            }
+
+            if (_audioSrc.isPlaying && _audioSrc.clip == applicableAudioLoop.Clip)
+            {
+                //We leave the audio to play when it matches the package already
+                continue;
+            }
+            else if (!_audioSrc.isPlaying && _audioSrc.clip == applicableAudioLoop.Clip)
+            {
+                _audioSrc.mute = false;
+                _audioSrc.Play();
+                StartCoroutine(AudioUtils.FadeInAudioToOne(_audioSrc, 1f));
+            }
+            else
+            {
+                StartCoroutine(AudioTransitionRoutine(_audioSrc, applicableAudioLoop));
+            }
+        }
+    }
+
+    private IEnumerator AudioTransitionRoutine(AudioSource audioSrc, AudioLoopPackage loopPkg)
+    {
+        if (!audioSrc.mute)
+        {
+            yield return StartCoroutine(AudioUtils.FadeOutAudioToZero(audioSrc, 0.5f));
+            audioSrc.mute = true;
+            audioSrc.Stop();
+        }
+        audioSrc.clip = loopPkg.Clip;
+        audioSrc.mute = false;
+        audioSrc.Play();
+        StartCoroutine(AudioUtils.FadeInAudioToOne(audioSrc, 1f));
     }
 }
--- Assets/Mymy/Scripts/System Scripts/GameManager.cs
+++ Assets/Mymy/Scripts/System Scripts/GameManager.cs
@@ -18,8 +18,11 @@ public enum SkyboxType
 
 public class GameManager : MonoBehaviour
 {
+    public delegate void SceneChange(DrugVR_SceneENUM nextScene);
+    public static event SceneChange OnSceneChange = null;
+
     /* constants */
-    
+
     private const string FOCUS_CONTROLLER_OBJECT_NAME = "/VIVEFocusWaveVR/FocusController";
     private const string HEAD_OBJECT_NAME = "/VIVEFocusWaveVR/head";
     
@@ -113,8 +116,8 @@ public class GameManager : MonoBehaviour
         }
         set { m_MenuControl = value; }
     }
-        
 
+    public event Action<VideoPlayer> OnSceneVideoEnd;
     //Only one instance of this videoplayer can be obtained and present in any scenes
     private VideoPlayer m_SkyVideoPlayer;
     public VideoPlayer SkyVideoPlayer
@@ -154,10 +157,6 @@ public class GameManager : MonoBehaviour
     public Material StillSkyMat;
 
     public bool FadeToBlack = true;
-
-    public event Action<VideoPlayer> OnSceneVideoEnd;
-    public delegate void SceneChange(DrugVR_SceneENUM nextScene);
-    public static event SceneChange OnSceneChange = null;
 
     /* end of public references */
 
@@ -219,7 +218,11 @@ public class GameManager : MonoBehaviour
 
         yield return StartCoroutine(Scribe.LoadTxtDataWWW());
 
-        StartCoroutine(ReadScroll(CurrentSceneScroll));       
+        StartCoroutine(ReadScroll(CurrentSceneScroll));
+        if (OnSceneChange != null)
+        {
+            OnSceneChange(CurrentScene);
+        }
     }
 
     private void FixedUpdate()
@@ -335,11 +338,11 @@ public class GameManager : MonoBehaviour
                 PauseVideo();
                 SceneManager.LoadScene(CurrentSceneScroll.SceneName);
 
-                ReadScroll(CurrentSceneScroll);
-                if (OnSceneChange != null)
-                {
-                    OnSceneChange(sceneEnum);
-                }
+                ReadScroll(CurrentSceneScroll);               
+            }
+            if (OnSceneChange != null)
+            {
+                OnSceneChange(sceneEnum);
             }
         }
 
